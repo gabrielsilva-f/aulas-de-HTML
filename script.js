a17b0d293768a05ce7d168c145e33b5f06071df0
gabrielsilva-f <chefin004@gmail.com>
2022-11-16 16:09:58 -0300

    atividade

---
+++ script.js
@@ -0,0 +1,33 @@
+/*function clickMe(){
+    console.log("Fui clicado!")
+}
+
+function overMe(){
+    console.log("Mouse aqui!")
+} 
+
+function Blur(){
+    console.log("borer")
+}
+
+function Focus(){
+     console.log("foca")
+}
+
+function Change(){
+     console.log("chance")
+}
+
+document.querySelector('#teste').innerHTML = "<h1>Bruno</h1>"*/
+
+function clicar(){
+   document.querySelector('.div1').innerText = "Neymar meu homi"
+}
+function passando(){
+    document.querySelector('.div1').innerText = "Neymar marca hat-trick"
+}
+
+function saindo(){
+    document.querySelector('.div1').innerText = "É essa Hexa "
+}
+
